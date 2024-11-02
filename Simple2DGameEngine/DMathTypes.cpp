2f267a26672acdbc97e4dd411d1f351016476feb
han._.Nari_ <luminous_crescent@naver.com>
2024-11-03 02:09:34 +0900

    feat : 백터 구조체 추가
    수학 관련 구조체 및 타입 등등을 작성하는 DMathTypes 파일

---
+++ Simple2DGameEngine/DMathTypes.cpp
@@ -0,0 +1,14 @@
+#include "DMathTypes.h"
+
+
+DVector2i::DVector2i()
+{
+	x = 0;
+	y = 0;
+}
+
+DVector2i::DVector2i(int x, int y)
+{
+	this->x = x;
+	this->y = y;
+}
---
+++ Simple2DGameEngine/DMathTypes.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+struct DVector2i
+{
+public:
+	DVector2i();
+	DVector2i(int x, int y);
+
+public:
+	int x;
+	int y;
+	
+};
