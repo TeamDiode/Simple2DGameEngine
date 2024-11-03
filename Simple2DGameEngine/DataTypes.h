c5a6622acf4e63f956550f2c90d674db3995fb9e
han._.Nari_ <luminous_crescent@naver.com>
2024-11-04 02:34:11 +0900

    feat : 어레이 자료구조 추가
    테스트 이전

--- Simple2DGameEngine/DataTypes.h
+++ Simple2DGameEngine/DataTypes.h
@@ -1,3 +1,21 @@
 #pragma once
 
 
+template <class MemberType>
+struct DArray
+{
+public:
+	DArray();
+	DArray(int capacity);
+	~DArray();
+
+private:
+	MemberType* members;
+	int capacity;
+
+public:
+	MemberType operator [](int value);
+	int GetCapacity();
+
+};
+
