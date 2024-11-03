d6d824fd57bcca0e0b9762fa2bdf836247982f8a
han._.Nari_ <luminous_crescent@naver.com>
2024-11-03 23:49:33 +0900

    feat : 오브젝트의 상태 프로퍼티 선언부 작성
    위치는 2차원 벡터
    각도는 1차원 유리수, 단위는 도(degree)
    크기는 2차원 벡터
    가도를 라디언 단위의 값으로 수정하는 기능 포함

--- Simple2DGameEngine/DObject.cpp
+++ Simple2DGameEngine/DObject.cpp
@@ -2,8 +2,50 @@
 
 DObject::DObject()
 {
+	location = DVector2i();
+	angle = 0.f;
+	scale = DVector2i();
 }
 
 DObject::~DObject()
+{
+}
+
+DVector2i DObject::GetLocation()
+{
+	return DVector2i();
+}
+
+void DObject::SetLocation(DVector2i newLocation)
+{
+}
+
+void DObject::SetLocation(int newLocationX, int newLocationY)
+{
+}
+
+float DObject::GetAngle()
+{
+	return 0.0f;
+}
+
+void DObject::SetAngle(float degree)
+{
+}
+
+void DObject::SetAngleByRadian(float radian)
+{
+}
+
+DVector2i DObject::GetScale()
+{
+	return DVector2i();
+}
+
+void DObject::SetScale(DVector2i newScale)
+{
+}
+
+void DObject::SetScale(int newScaleX, int newScaleY)
 {
 }
--- Simple2DGameEngine/DObject.h
+++ Simple2DGameEngine/DObject.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "DMathTypes.h"
 
 
 class DObject
@@ -7,5 +8,37 @@ class DObject
 public:
 	DObject();
 	~DObject();
+
+private:
+	// 위치
+	DVector2i location;
+	// 각도. 단위는 도(degree)
+	float angle;
+	// 크기. 정규값은 (1, 1)
+	DVector2i scale;
+	
+public:
+	/* 오브젝트 상태 프로퍼티 */
+
+	// 위치을 반환
+	DVector2i GetLocation();
+	// 위치를 벡터로 수정
+	void SetLocation(DVector2i newLocation);
+	// 위치를 원소로 수정
+	void SetLocation(int newLocationX, int newLocationY);
+
+	// 각도를 반환
+	float GetAngle();
+	// 각도를 도로 수정
+	void SetAngle(float degree);
+	// 각도를 라디언으로 수정
+	void SetAngleByRadian(float radian);
+
+	// 크기를 반환
+	DVector2i GetScale();
+	// 크기를 벡터로 수정
+	void SetScale(DVector2i newScale);
+	// 크기를 원소로 수정
+	void SetScale(int newScaleX, int newScaleY);
 };
 
