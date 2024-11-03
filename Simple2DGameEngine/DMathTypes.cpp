1fe750bcd06c7fda1345bab869c619817f03ad23
han._.Nari_ <luminous_crescent@naver.com>
2024-11-04 00:31:08 +0900

    feat : 벡터 연산자 추가
    벡터 + 벡터
    벡터 - 벡터
    벡터 * 스칼리
    내적과 외적은 아직

--- Simple2DGameEngine/DMathTypes.cpp
+++ Simple2DGameEngine/DMathTypes.cpp
@@ -12,3 +12,18 @@ DVector2i::DVector2i(int x, int y)
 	this->x = x;
 	this->y = y;
 }
+
+DVector2i DVector2i::operator+(DVector2i value)
+{
+	return DVector2i(x + value.x, y + value.y);
+}
+
+DVector2i DVector2i::operator-(DVector2i value)
+{
+	return DVector2i(x - value.x, y - value.y);
+}
+
+DVector2i DVector2i::operator*(int value)
+{
+	return DVector2i(x * value, y * value);
+}
--- Simple2DGameEngine/DMathTypes.h
+++ Simple2DGameEngine/DMathTypes.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// TODO : ³»Àû°ú ¿ÜÀû¿¬»ê Ãß°¡
 
 struct DVector2i
 {
@@ -8,7 +9,17 @@ public:
 	DVector2i(int x, int y);
 
 public:
+	// x ¿ø¼Ò
 	int x;
+	// y ¿ø¼Ò
 	int y;
+
+public:
+	// º¤ÅÍ µ¡¼À
+	DVector2i operator +(DVector2i value);
+	// º¤ÅÍ »¬¼À
+	DVector2i operator -(DVector2i value);
+	// ½ºÄ®¶ó °ö
+	DVector2i operator *(int value);
 	
 };
