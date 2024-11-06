461c1481aa8811422091bab05cc743aa7155f9f4
han._.Nari_ <luminous_crescent@naver.com>
2024-11-06 17:45:24 +0900

    feat : 링크드 리스트 클래스 추가

--- Simple2DGameEngine/DLinkedList.hpp
+++ Simple2DGameEngine/DLinkedList.hpp
@@ -1,1 +1,55 @@
 #pragma once
+
+#include "IDIterator.hpp"
+
+template<typename MemberType>
+class LinkedList : public IDIterator<MemberType>
+{
+public:
+	LinkedList();
+	LinkedList(MemberType newValue);
+	LinkedList(MemberType newValue, LinkedList* nextNode);
+	~LinkedList();
+
+private:
+	MemberType value;
+	LinkedList* next;
+
+public:
+	virtual void MoveNext() override;
+	virtual MemberType GetValue() override;
+
+};
+
+template<typename MemberType>
+inline LinkedList<MemberType>::LinkedList()
+{
+	value = MemberType();
+}
+
+template<typename MemberType>
+inline LinkedList<MemberType>::LinkedList(MemberType newValue)
+{
+	value = newValue;
+}
+
+template<typename MemberType>
+inline LinkedList<MemberType>::LinkedList(MemberType newValue, LinkedList* nextNode)
+{
+}
+
+template<typename MemberType>
+inline LinkedList<MemberType>::~LinkedList()
+{
+}
+
+template<typename MemberType>
+inline void LinkedList<MemberType>::MoveNext()
+{
+}
+
+template<typename MemberType>
+inline MemberType LinkedList<MemberType>::GetValue()
+{
+	return value;
+}
