28228a248bd30effc63eb9e5d353914471b705c5
leuneoe25 <leuneoe97@gmail.com>
2024-11-08 00:24:17 +0900

    feat : Math 함수 추가
    - Min
    - Max
    - Clamp
    - 삼각함수
    - 역삼각함수

--- Simple2DGameEngine/DMathHelper.h
+++ Simple2DGameEngine/DMathHelper.h
@@ -1,6 +1,19 @@
 #pragma once
 class DMathHelper
 {
+public:
+	static float Min(float a, float b);
+	static float Max(float a, float b);
+	static float Clamp(float a, float min, float max);
+	
+	static float Sin(float val);
+	static float Cos(float val);
+	static float Tan(float val);
+
+	static float Asin(float val);
+	static float Acos(float val);
+	static float Atan(float val);
+
 
 };
 
