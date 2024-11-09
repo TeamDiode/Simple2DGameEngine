8084656845f67e37787220030e81c43b3efc3438
Blueberry-0321 <juyeongim369@gmail.com>
2024-11-09 20:57:07 +0900

    feat : physics Manager 추가
    리스트로 오브젝트를 관리.
    오브젝트의 상태(중력 적용, 위치 갱신)를 업데이트. 오브젝트 간의 충돌 감지 및 해결 기능.

---
+++ Simple2DGameEngine/DPhysicsManager.cpp
@@ -0,0 +1,29 @@
+#include "DPhysicsManager.h"
+
+void DPhysicsManager::AddObject(DCollisionData* object) {
+    objects.AddNext(object); // 객체 추가
+}
+
+void DPhysicsManager::UpdateObjects(float deltaTime) {
+    for (int i = 0; i < objects.GetSize(); i++) {
+        DCollisionData* obj = objects.GetValue();
+        gravity.ApplyGravity(*obj, deltaTime); // 중력 적용
+        obj->UpdatePosition(deltaTime); // 위치 업데이트
+        objects.Move();
+    }
+}
+
+void DPhysicsManager::ResolveCollisions() {
+    for (int i = 0; i < objects.GetSize(); i++) {
+        DCollisionData* objA = objects.GetValue();
+        for (int j = i + 1; j < objects.GetSize(); j++) {
+            DCollisionData* objB = objects.GetValue();
+            CollisionDetector detector(objA, objB);
+            if (detector.detectCollision()) {
+                detector.ResolveCollision(*objA, *objB); // 충돌 해결
+            }
+            objects.Move();
+        }
+        objects.Move();
+    }
+}
