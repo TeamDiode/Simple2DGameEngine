2a42b3b59b8c7712fc3c51cf798b3392b4607180
han._.Nari_ <luminous_crescent@naver.com>
2024-11-11 19:04:57 +0900

    feat : 샘플 플레이어 클래스에 키 입력을 받는 코드 추가
    샘플 맵 클래스에서 초기화 관련 코드 추가

--- Simple2DGameEngine/SimpleMapTool.cpp
+++ Simple2DGameEngine/SimpleMapTool.cpp
@@ -10,4 +10,13 @@ void SimpleMapTool::Activated()
 {
 	DObject* a = DObjectManager::CreateObject(TestPlayer());
 	a->SetLocation(500, 500);
+	a->SetScale(100, 100);
+}
+
+void TestPlayer::Update()
+{
+	if (DInputManager::GetKeyDown(D))
+	{
+		SetLocation(GetLocation() + DVector2i(0, 10));
+	}
 }
