00637f477d030cb78b078ced666ff4b3f57cb558
Blueberry-0321 <juyeongim369@gmail.com>
2024-11-11 18:30:03 +0900

    feat : mass 계산 함수 추가
    사각형, 원에 따른 질량 계산식 추가

--- Simple2DGameEngine/DCollisionData.cpp
+++ Simple2DGameEngine/DCollisionData.cpp
@@ -1,10 +1,29 @@
 #include "DCollisionData.h"
 
-DCollisionData::DCollisionData(const DVector2i& position, const DVector2i& s, float m, float r)
-    : pos(position), size(size), mass(m), restitution(r) {
+DCollisionData::DCollisionData(const DVector2i& position, const DVector2i& s, Shape sha, float den, float r)
+    : pos(position), size(size), shape(sha), density(den), restitution(r) {
     //AABB경계 구하기
     aabb.min = pos - (size * 0.5f);
     aabb.max = pos + (size * 0.5f);
+
+    mass = CalculateMass();
+}
+
+float DCollisionData::CalculateMass()
+{
+    if (shape == Shape::Rectangle)
+    {
+        float area = size.x * size.y;
+        return density * area;
+    }
+    else if (shape == Shape::Circle)
+    {
+        float radius = size.x / 2.0f; // 반지름
+        float area = 3.141592f * radius * radius * size.y;
+        return density * area;
+    }
+    return 0.0f; // 다른 형태
+    
 }
 
 void DCollisionData::UpdatePosition(float deltaTime) {
--- Simple2DGameEngine/DCollisionData.h
+++ Simple2DGameEngine/DCollisionData.h
@@ -2,18 +2,28 @@
 
 #include "DMathTypes.h"
 
+enum class Shape
+{
+    Rectangle, 
+    Circle
+};
+
 class DCollisionData {
 public:
     DVector2i pos;      // 물체의 위치
     DVector2i velocity; // 물체의 속도
     DVector2i size;
-    float mass; // 질량. (일단 넣어놨지만 사용은 안함.)
+    float mass; // 질량. 
+    float density; // 밀도
+    Shape shape;
     float restitution; // 반발 계수
     AABB aabb; // 물체의 AABB 충돌 경계
 
 public:
-    DCollisionData(const DVector2i& position, const DVector2i& size, float m, float r);
+    DCollisionData(const DVector2i& position, const DVector2i& size, Shape sha, float den, float r);
+    float CalculateMass();
     void UpdatePosition(float deltaTime);
+
 };
 
 struct AABB
