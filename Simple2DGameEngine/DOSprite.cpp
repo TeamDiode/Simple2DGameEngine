787b75c2bc32cdb4dfb62658a65ae6a24ddabca6
GDH05 <rhehgus9466@gmail.com>
2024-11-12 19:03:07 +0900

    chore : 회전값 수정
    24.11.12

--- Simple2DGameEngine/DOSprite.cpp
+++ Simple2DGameEngine/DOSprite.cpp
@@ -4,63 +4,58 @@
 #include "DMathTypes.h"
 #include <Windows.h>
 
-#define LEFT GetLocation().x - GetScale().x / 2
-#define RIGHT GetLocation().x + GetScale().x / 2
-#define TOP GetLocation().y - GetScale().y / 2
-#define BOTTOM GetLocation().y + GetScale().y / 2
+#define PI 3.14
+
+#define LEFT (GetLocation().x - GetScale().x / 2)
+#define RIGHT (GetLocation().x + GetScale().x / 2)
+#define TOP (GetLocation().y - GetScale().y / 2)
+#define BOTTOM (GetLocation().y + GetScale().y / 2)
 
 DOSprite::DOSprite()
 {
-	type = 1;
+    type = 1;
+    angleInRadian = GetAngle() * (PI / 180.0f);
 }
 
 DOSprite::DOSprite(int newType)
 {
-	type = newType;
-	DRenderer::RegisterSprite(this);
+    type = newType;
+    DRenderer::RegisterSprite(this);
+}
+
+DVector2i DOSprite::GetRotatedPosition(float x, float y)
+{
+    DVector2i center = GetLocation();
+
+    //변경된 x
+    int rotatedX = center.x + ((x - center.x) * cos(angleInRadian) - (y - center.y) * sin(angleInRadian));
+    //변경된 y
+    int rotatedY = center.y + ((x - center.x) * sin(angleInRadian) + (y - center.y) * cos(angleInRadian));
+    
+    return DVector2i{ rotatedX, rotatedY };
 }
 
 DVector2i DOSprite::GetLeftTopPosition()
 {
-	//left
-	position.x = LEFT * cos(GetAngle()) - TOP * sin(GetAngle());
-	//top
-	position.y = LEFT * sin(GetAngle()) + TOP * cos(GetAngle());
-
-	return position;
+    return GetRotatedPosition(LEFT, TOP);
 }
 
 DVector2i DOSprite::GetLeftBottomPosition()
 {
-	//left
-	position.x = LEFT * cos(GetAngle()) - BOTTOM * sin(GetAngle());
-	//bottom
-	position.y = LEFT * sin(GetAngle()) + BOTTOM * cos(GetAngle());
-
-	return position;
+    return GetRotatedPosition(LEFT, BOTTOM);
 }
 
 DVector2i DOSprite::GetRightTopPosition()
 {
-	//right
-	position.x = RIGHT * cos(GetAngle()) - TOP * sin(GetAngle());
-	//top
-	position.y = RIGHT * sin(GetAngle()) + TOP * cos(GetAngle());
-
-	return position;
+    return GetRotatedPosition(RIGHT, TOP);
 }
 
 DVector2i DOSprite::GetRightBottomPosiiton()
 {
-	//right
-	position.x = RIGHT * cos(GetAngle()) - BOTTOM * sin(GetAngle());
-	//bottom
-	position.y = RIGHT * sin(GetAngle()) + BOTTOM * cos(GetAngle());
-
-	return position;
+    return GetRotatedPosition(RIGHT, BOTTOM);
 }
 
 int DOSprite::GetType()
 {
-	return type;
+    return type;
 }
