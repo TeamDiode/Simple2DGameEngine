0d781d04bd6e21499c798c607c6bf498bec84852
Blueberry-0321 <juyeongim369@gmail.com>
2024-11-26 09:26:08 +0900

    feat : 부모자식 설정, 반발력 입력 추가
    오브젝트에 부모자시ㄱ관계 설정.
    반발력 입력 받아 설정

--- Simple2DGameEngine/DPhysicsManager.h
+++ Simple2DGameEngine/DPhysicsManager.h
@@ -3,6 +3,7 @@
 #include "DMathTypes.h"
 #include "DGravity.h"
 #include "DList.hpp"
+#include "DCollisionData.h"
 
 class DPhysicsManager {
 public:
@@ -13,7 +14,17 @@ public:
     // 충돌 처리
     void ResolveCollisions();
 
+    // 부모 관계 설정
+    void SetParent(DCollisionData* parent, DCollisionData* child);
+    // 부모 제거
+    void RemoveParent(DCollisionData* child);
+    // 반발 계수 설정
+    void SetRestitutionForObject(DCollisionData* object, float restitutionValue) {
+        object->SetRestitution(restitutionValue);
+    }
+
 private:
     static DList<DCollisionData*> objects;
     DGravity gravity; // 중력 관리
+    float restitutionStrength;
 };
