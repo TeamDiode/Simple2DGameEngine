7855a55003cf427efbc5f269e334724b13a333bf
leuneoe25 <leuneoe97@gmail.com>
2024-12-01 03:38:40 +0900

    feat : EnemyManager 구현
     - 주석
     - 일정 시간마다 적 생성하는 EnemyManager  구현

--- Simple2DGameEngine/DWorld.h
+++ Simple2DGameEngine/DWorld.h
@@ -5,6 +5,7 @@
 #include "DInput.h"
 
 #include <iostream>
+#include <random>
 #include <queue>
 using namespace std;
 
@@ -18,7 +19,9 @@ public:
 	virtual void Load();
 };
 
-
+// 총알 클래스
+// - 플레이어와 마우스의 뱡향 계산 후 이동
+// - 일정 시간 후 삭제
 class Bullet : public DObject
 {
 public:
@@ -26,15 +29,17 @@ public:
 	~Bullet() { delete skin; }
 private:
 	DOSprite* skin;
+	// 총알 속도
 	float currentSpeed;
+	// 나아가야할(마우스 위치) 방향을 저장하는 벡터
 	DVector2i dir;
 
+	// 총알을 삭제를 위한 시간 변수
 	float curTime;
 	float delTime;
 public:
 	virtual void Update(double deltaTime) override
 	{
-		//currentSpeed += deltaTime * acceleration;
 		// 구한 마우스 방향으로 이동
 		SetLocation(GetLocation() + (dir * deltaTime * currentSpeed));
 
@@ -45,6 +50,9 @@ public:
 	}
 };
 
+// 플래이어 클래스
+// - 이동 기능
+// - 총알 생성
 class Player : public DObject
 {
 public:
@@ -98,10 +106,13 @@ public:
 	}
 };
 
+// 적 클래스
+// - 이동기능
+// 이후 충돌판정 후 삭제 기능
 class Enemy : public DObject
 {
 public:
-	Enemy()
+	Enemy(DObject* obj) : player(obj)
 	{
 		DOSprite* skin = new DOSprite(1);
 		skin->SetScale(70, 70);
@@ -119,14 +130,15 @@ public:
 	}
 private:
 	DOSprite* skin;
+	// 플레이어 위치를 저장하는 변수
 	DObject* player;
+	// 적 HP
 	int curHp;
 	// 플레이어 인식 범위
 	float cognizance;
 	// 이동속도
 	float moveSpeed;
 public:
-	void SetPlayer(DObject* p) { player = p; };
 	virtual void Update(double deltaTime) override
 	{
 		if (player == nullptr) return;
@@ -145,20 +157,47 @@ public:
 	// 충돌 판정 후 hp
 }; 
 
+// 적 생성을 관리하는 클래스
+// - 일정 시간마다 적 생성
 class EnemyManager : public DObject
 {
 public:
-	EnemyManager() {}
+	EnemyManager(DObject* obj) :gen(), distr(100, 600), playerObj(obj)
+	{
+		regenerateTime = 10;
+		curTime = regenerateTime;
+	}
 private:
-	Player* playerObj;
+	// 플레이어를 저장하는 오브젝트
+	DObject* playerObj;
+	// 리젠을 위한 시간 변수
+	float curTime; // 현재 시간
+	float regenerateTime; // 리젠 시간
+	// 랜덤을 위한 클래스
+	random_device rd;
+	mt19937 gen;
+	uniform_int_distribution<int> distr;
 public:
+	virtual void Update(double deltaTime) override
+	{
+		// 현재 시간 계산
+		curTime += deltaTime;
+
+		// 만약 현재 시간이 리젠 시간과 같거나 크다면
+		if (curTime >= regenerateTime)
+		{
+			// 적 생성
+			CreataEnemy();
+			// 현재 시간 초기화
+			curTime = 0;
+		}
+	}
 	// 적 생성 함수
 	void CreataEnemy()
 	{
 		// 적 생성
-		Enemy* e = new Enemy();
-		e->SetLocation(100, 100);
-		e->SetPlayer(playerObj);
+		Enemy* e = new Enemy(playerObj);
+		e->SetLocation(distr(gen), distr(gen));
 	}
 
 
