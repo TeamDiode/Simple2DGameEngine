0894d0bcd7bf86480f460f6e7d66c951d0f39282
leuneoe25 <leuneoe97@gmail.com>
2024-12-01 03:56:23 +0900

    style : FlappyBird 주석 처리

--- Simple2DGameEngine/DWorld.cpp
+++ Simple2DGameEngine/DWorld.cpp
@@ -10,42 +10,57 @@ DWorld::~DWorld()
 
 void DWorld::Load()
 {
-	Player* a = new Player();
-	a->SetLocation(200, 500);
+	// 플레이어 생성
+	Player* player = new Player();
+	// 플레이어 위치 초기화
+	player->SetLocation(200, 500);
 
+	// 벽 관리 클래스 생성
 	WallManager* wallManager = new WallManager();
 	
 }
 
 Wall::Wall() : DCollisionData(Shape::Rectangle, 1, 1)
 {
+	// 이미지 오브젝트 생성
 	DOSprite* skin = new DOSprite(1);
+	// 오브젝트 크기 설정
 	skin->SetScale(100, 400);
+	// 하위 오브젝트에 추가
 	DObject::AttachObject(skin);
+	// DCollisionData 중력 또는 속도로 인한 X Y 축 이동 잠금
 	freezeX = true;
 	freezeY = true;
+	// 사용되기 전으로 초기화
 	isUsed = false;
 
+	// 삭제 시간과 현재 시간을 초기화
 	destoryTime = 5;
 	curTime = 0;
 }
 
 Wall::~Wall()
 {
+	// 이미지 오브젝트 해제
 	delete skin;
 }
 
 void Wall::Update(double deltaTime)
 {
+	// 사용된 벽이라면 활동 중지
 	if (isUsed) return;
 
-
+	// 벽 이동
 	SetLocation(GetLocation() + DVector2i(deltaTime * -200, 0));
 
+	// 현재 시간 계산
 	curTime += deltaTime;
+	// 만약 현재 시간이 삭제 시간보다 크거나 같다면
 	if (curTime >= destoryTime)
 	{
+		// 현재 시간을 0으로 초기화
 		curTime = 0;
+		// 사용되었다고 설정
 		isUsed = true;
 	}
 
@@ -53,45 +68,65 @@ void Wall::Update(double deltaTime)
 
 Wall* WallObjectPool::GetWall()
 {
+	// 만약 큐의 맨 앞이 사용 가능하다면
+	if (PossibleGetWall())
 	{
-		if (PossibleGetWall())
-		{
-			Wall* data = objQueue.front();
-			objQueue.pop();
-			objQueue.push(data);
-			return data;
-		}
-		else
-		{
-			Wall* wall = new Wall();
-			objQueue.push(wall);
-			return wall;
-		}
+		// 큐의 맨 앞을 가져오기
+		Wall* data = objQueue.front();
+		// 큐에서 맨 앞 데이터 삭제
+		objQueue.pop();
+		// 맨 앞에 있던 데이터 맨 뒤로 보내기
+		objQueue.push(data);
+		// 맨 앞에 있었던 벽 반환
+		return data;
+	}
+	else
+	{
+		// 맨앞의 큐가 사용 불가능하다면
+		// 1. 큐가 비어 있거나 
+		// 2. 맨 앞의 큐가 사용중
+
+		// 벽 오브젝트 생성
+		Wall* wall = new Wall();
+		// 큐에 벽 저장
+		objQueue.push(wall);
+		// 생성된 벽 반환
+		return wall;
 	}
 }
 
 bool WallObjectPool::PossibleGetWall()
 {
+	// 큐가 비어있다면 false 반환
 	if (objQueue.empty()) return false;
+	// 맨 앞 큐가 사용 가능하다면 true 반환
 	return objQueue.front()->GetIsUsed();
 }
 
 void WallManager::Update(double deltaTime)
 {
+	// 현재 시간 계산
 	curTime += deltaTime;
+
 	if (curTime >= createWallTime)
 	{
+		// 벽 2개 추가
+		// 위 벽
 		Wall* wall = wop.GetWall();
+		// 아랫 벽
 		Wall* wall2 = wop.GetWall();
 
-
+		// 벽 초기화
 		wall->Reset();
 		wall2->Reset();
 
+		// 랜덤으로 벽 위치 조정
 		float dis = distr(gen);
 
+		// 일정한 거리가 되도록 윗 벽, 아랫 벽에 랜덤 값 연산
 		wall->SetLocation(700, 125 + dis);
 		wall2->SetLocation(700, 650 + dis);
+		// 현재 시간을 0으로 초기화
 		curTime = 0;
 	}
 
@@ -99,19 +134,24 @@ void WallManager::Update(double deltaTime)
 
 Player::Player()
 {
+	// 이미지 오브젝트 생성
 	DOSprite* skin = new DOSprite(1);
+	// 이미지 크기 설정
 	skin->SetScale(70, 70);
+	// 이미지 오브젝트 플레이어 오브젝트 하위에 상속
 	DObject::AttachObject(skin);
 }
 
 void Player::Update(double deltaTime)
 {
+	// 만약 space 키를 눌렀을 때 캐릭터 위치 위로 이동
 	if (DInputManager::GetKey(space))
 	{
 		SetLocation(GetLocation() + DVector2i(0, deltaTime * -200));
 	}
-	else
+	else // 그렇지 않다면
 	{
+		// 아래로 이동
 		SetLocation(GetLocation() + DVector2i(0, deltaTime * 200));
 	}
 
--- Simple2DGameEngine/DWorld.h
+++ Simple2DGameEngine/DWorld.h
@@ -7,6 +7,7 @@
 #include "DCollisionData.h"
 
 #include <iostream>
+#include <random>
 #include <queue>
 using namespace std;
 
@@ -19,7 +20,9 @@ public:
 public:
 	virtual void Load();
 };
-
+// 벽 클래스
+// - 오브젝트 이동
+// - 일정 시간 후 비활성화
 class Wall : public DCollisionData
 {
 public:
@@ -27,39 +30,55 @@ public:
 	~Wall();
 private:
 	DOSprite* skin;
+	// 삭제를 위한 시간을 계산할 변수
 	float curTime;
 	float destoryTime;
+	// 이용된 벽인지 확인하는 변수
 	bool isUsed;
 public:
 	virtual void Update(double deltaTime) override;
+	// 이미 사용된 벽인지 확인
 	bool GetIsUsed() { return isUsed; }
+	// 다시 사용하기 위해 초기화
 	void Reset() { isUsed = false; }
 };
+// 벽을 재사용하기 위한 오브젝트풀
+// - 오브젝트풀 패턴 구현
 class WallObjectPool
 {
 public:
 	WallObjectPool() {}
 private:
+	// 벽들을 저장할 자료구조 큐
 	queue<Wall*> objQueue;
 public:
+	// 사용할 벽을 반환하는 함수
 	Wall* GetWall();
+	// 현재 큐의 맨 앞에 벽이 사용가능한지 확인하는 함수
 	bool PossibleGetWall();
+	// 큐의 크기(== 벽의 갯수)를 확인하는 함수
 	int GetSize() { return objQueue.size(); }
 };
-#include <random>
+
+// 벽을 관리하는 클래스
+// - 벽 생성 또는 벽 초기화
 class WallManager : public DObject
 {
 public:
 	WallManager() :gen(), distr(-90, 90)
 	{
 		createWallTime = 2;
+		// 맨 처음부터 오브젝트를 생성하기 위해 초기화
 		curTime = createWallTime;
 	}
 private:
+	// 벽을 재사용 또는 초기화하는데 시간 계산하는 변수
 	float curTime;
 	float createWallTime;
+	// 오브젝트 풀 클래스
 	WallObjectPool wop;
 
+	// 랜덤 구현을 위한 클래스
 	random_device rd;
 	mt19937 gen;
 	uniform_int_distribution<int> distr;
@@ -72,10 +91,8 @@ class Player :public DObject
 {
 public:
 	Player() /*: DCollisionData(Shape::Rectangle,1,1)*/;
-	~Player()
-	{
-		delete skin;
-	}
+	// 이미지 오브젝트 해제
+	~Player() { delete skin; }
 private:
 	DOSprite* skin;
 public:
