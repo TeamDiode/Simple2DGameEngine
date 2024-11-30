1e9d4e85529a73163337d1668710bf1ed5605da1
leuneoe25 <leuneoe97@gmail.com>
2024-12-01 03:22:05 +0900

    feat : Enemy 간단한 AI 구현

--- Simple2DGameEngine/DWorld.cpp
+++ Simple2DGameEngine/DWorld.cpp
@@ -10,32 +10,32 @@ DWorld::~DWorld()
 
 void DWorld::Load()
 {
+	// 플레이어 생성
 	Player* a = new Player();
 	a->SetLocation(500, 500);
 	a->SetScale(100, 100);
+
+	
 }
 #include "DEngine.h"
 #include <string>
 Bullet::Bullet(DVector2i pos)
 {
+	// 현재 위치 설정 ( 플레이어 위치 )
 	SetLocation(pos);
 
-	acceleration = 100;
-	currentSpeed = 500;
+	currentSpeed = 100;
 	DOSprite* skin = new DOSprite(2);
 	skin->SetScale(50, 50);
 	AttachObject(skin);
+	
+	// 삭제 시간과 생성된 후 시간을 저장할 변수 초기화
+	curTime = 0;
+	delTime = 1;
 
-
+	// 마우스 방향 계산
 	dir = DInputManager::GetMousePostion();
 	dir = dir - GetLocation();
 	dir.Normalize();
-
-
-	
-	/*std::string commend1 = "Mouse : " + std::to_string(DInputManager::GetMousePostion().x) + "," + std::to_string(DInputManager::GetMousePostion().y);
-	std::string commend2 = "Player : " + std::to_string(pos.x) + "," + std::to_string(pos.y);
-	std::string commend3 = "Reuslt : " + std::to_string(dir.x) + "," + std::to_string(dir.y);
-	std::string temp = commend1 + "\n" + commend2 + "\n" + commend3;
-	DEngine::LogMessageBox(temp.c_str());*/
 }
+
--- Simple2DGameEngine/DWorld.h
+++ Simple2DGameEngine/DWorld.h
@@ -4,6 +4,9 @@
 #include "DRenderer.h"
 #include "DInput.h"
 
+#include <iostream>
+#include <queue>
+using namespace std;
 
 class DWorld
 {
@@ -15,22 +18,30 @@ public:
 	virtual void Load();
 };
 
+
 class Bullet : public DObject
 {
 public:
 	Bullet(DVector2i pos);
-
+	~Bullet() { delete skin; }
 private:
 	DOSprite* skin;
-	float acceleration;
 	float currentSpeed;
 	DVector2i dir;
+
+	float curTime;
+	float delTime;
 public:
 	virtual void Update(double deltaTime) override
 	{
 		//currentSpeed += deltaTime * acceleration;
-
+		// 구한 마우스 방향으로 이동
 		SetLocation(GetLocation() + (dir * deltaTime * currentSpeed));
+
+		curTime += deltaTime;
+		// 생성된 후 일정 시간 후 스스로 파괴
+		if (curTime >= delTime)
+			delete this;
 	}
 };
 
@@ -40,7 +51,7 @@ public:
 	Player()
 	{
 		DOSprite* skin = new DOSprite(1);
-		skin->SetScale(100, 100);
+		skin->SetScale(60, 60);
 		AttachObject(skin);
 		f = false;
 	}
@@ -54,7 +65,7 @@ private:
 public:
 	virtual void Update(double deltaTime) override
 	{
-		
+		// 이동 입력
 		if (DInputManager::GetKey(W))
 		{
 			SetLocation(GetLocation() + DVector2i(0, deltaTime * -200));
@@ -74,8 +85,9 @@ public:
 		/*if (DInputManager::GetMouseButton(0))*/
 		if (DInputManager::GetMouseButton(0))
 		{
-			if(!f)
-			Bullet* a = new Bullet(GetLocation());
+			// 총알 생성
+			if (!f)
+				Bullet* a = new Bullet(GetLocation());
 			f = true;
 			//SetLocation(DInputManager::GetMousePostion());
 		}
@@ -88,5 +100,66 @@ public:
 
 class Enemy : public DObject
 {
+public:
+	Enemy()
+	{
+		DOSprite* skin = new DOSprite(1);
+		skin->SetScale(70, 70);
+		AttachObject(skin);
+
+		curHp = 2;
+
+		cognizance = 300;
+
+		moveSpeed = 100;
+	}
+	~Enemy()
+	{
+		delete skin;
+	}
+private:
+	DOSprite* skin;
+	DObject* player;
+	int curHp;
+	// 플레이어 인식 범위
+	float cognizance;
+	// 이동속도
+	float moveSpeed;
+public:
+	void SetPlayer(DObject* p) { player = p; };
+	virtual void Update(double deltaTime) override
+	{
+		if (player == nullptr) return;
+		// 플레이어 오브젝트 거리와 자신과의 오브젝트 거리 계산
+		// 인식범위 안이라면
+		if (DVector2i::Distance(player->GetLocation(), GetLocation()) < cognizance)
+		{
+			// 플레이어와의 방향벡터 계산
+			DVector2i dir = player->GetLocation() - GetLocation();
+			dir.Normalize();
+
+			// 플레이어 쪽으로 이동
+			SetLocation(GetLocation() + (dir * deltaTime * moveSpeed));
+		}
+	}
+	// 충돌 판정 후 hp
+}; 
+
+class EnemyManager : public DObject
+{
+public:
+	EnemyManager() {}
+private:
+	Player* playerObj;
+public:
+	// 적 생성 함수
+	void CreataEnemy()
+	{
+		// 적 생성
+		Enemy* e = new Enemy();
+		e->SetLocation(100, 100);
+		e->SetPlayer(playerObj);
+	}
+
 
 };
