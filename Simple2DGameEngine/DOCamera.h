2c1681b66ee1a3a7dca9891f3f9aee583d3f857c
han._.Nari_ <luminous_crescent@naver.com>
2024-12-01 07:48:35 +0900

    feat : 임시 시뮬레이션 세계 내 카메라 이동

--- Simple2DGameEngine/DOCamera.h
+++ Simple2DGameEngine/DOCamera.h
@@ -11,11 +11,16 @@ public:
 	void Rendering(DList<DOSprite*> objects);
 	void Move(DVector2i position);
 
+	DVector2i GetSimulationLocation();
+	void SetSimulationLocation(DVector2i newLocation);
+
 private:
 	HWND hWnd;
 	HDC hdc;
 	RECT renderingRect;
 	DList<DOSprite*> objectList;
+
+	DVector2i simulationLocation;
 
 	void DrawScreen();
 	void DistanceCalculation(DOSprite* object);
